bec20f0aa8b62eea61be78b7cc65d99a3a01b3d7
mehedi-dt <mehedi.gazi.me@gmail,com>
2025-04-18 01:20:21 +0100

    added new env (global) and defined backed(s3)

---
+++ envs/global/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.4.0"
+  
+  # for using aws s3 for backend
+  backend "s3" {
+    bucket         = "bucket-name"
+    key            = "envs/global/terraform.tfstate"
+    region         = "ap-south-1" # region where the bucket is
+    dynamodb_table = "dynamodb-table-for-locking" # create a table with a Partition Key: LockID (type: String)
+    encrypt        = true
+    profile        = "profile-name-for-aws-secret-config" # not needed if using role-based access.
+  }
+}
